df6219df785d2fe4e993bd460d8143e650396ee7
Cribe Pencheff <cribe@self-made.se>
2025-01-18 20:47:31 +0100

    Logic, delete single todo

--- src/components/TodoList.ts
+++ src/components/TodoList.ts
@@ -1,19 +1,37 @@
+import { supabase } from '../services/supabaseClient';
 import { Todo } from '../types/app.types';
 import { getUserTodos } from '../utils/getUserTodos';
+import { deleteTodo } from '../utils/todosService';
 
 export const showTodos = async () => {
-
+  const { data } = await  supabase.auth.getUser();
+  const userId = data.user?.id;
   const main = document.getElementById('todo-list') as HTMLElement;
   const ul = document.createElement("ul");
   const fetchedTodos: Todo[] = await getUserTodos();
   let hasTodos = fetchedTodos.length;
 
   if (hasTodos) {
-    const myTodosHTML = fetchedTodos.map(todo => `
-      <li>${todo.title}</li>
+    ul.innerHTML = fetchedTodos.map(todo => `
+      <li data-id="${todo.id}">
+        ${todo.title}
+        <button class="delete-todo">DEL</button>
+      </li>
     `).join("");
 
-    ul.innerHTML = myTodosHTML;
+    ul.querySelectorAll('.delete-todo').forEach((deleteBtn) => {
+      deleteBtn.addEventListener('click', async (e) => {
+        const li = (e.target as HTMLElement).closest('li');
+        const todoId = parseInt(li?.getAttribute('data-id')!);
+        const confirmDelete = confirm(`Are you sure you want to remove this Todo?`);
+
+        if (todoId && userId && confirmDelete) {
+          await deleteTodo(todoId, userId);
+        }
+      })
+    });
+
+
   } else {
     ul.innerHTML = `<li>List empty, please add a todo</li>`
   }
--- src/utils/todosService.ts
+++ src/utils/todosService.ts
@@ -5,7 +5,6 @@ import { Todo } from '../types/app.types';
 export const createTodo = async (title: string): Promise<Todo> => {
   const { data } = await  supabase.auth.getUser();
 
-  // Skapa en ny todo
   const { data: todoData, error } = await supabase
     .from('todos')
     .insert([
@@ -23,5 +22,21 @@ export const createTodo = async (title: string): Promise<Todo> => {
   }
 
   showTodos();
-  return todoData as Todo; // Returnera den skapade todo
+  return todoData as Todo;
+};
+
+export const deleteTodo = async (todoId: number, userId: string): Promise<boolean> => {
+  const { error } = await supabase
+    .from('todos')
+    .delete()
+    .eq('user_id', userId)
+    .eq('id', todoId);
+
+  if (error) {
+    console.error('Error deleting todo:', error.message);
+    return false;
+  }
+
+  showTodos();
+  return true;
 };
